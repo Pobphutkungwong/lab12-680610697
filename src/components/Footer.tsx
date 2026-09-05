b255f21c50ad370479b6ca509780bb37dedb91b7
Pobphut <phopphut@gmail.com>
2026-09-05 18:08:05 +0700

    complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 /*
  * อย่าลืมเปลี่ยน fullName และ studentId ให้เป็นของตัวเอง
  */
-export default function Footer() {
+export default function Footer(props: {year:number, fullname:string, studentId:string}) {
   return (
     <footer className="text-center w-100">
-      <p className="text-white bg-secondary p-4 m-0">Copyright © TODO</p>
+      <p className="text-white bg-secondary p-4 m-0">Copyright © {props.year} {props.fullname} {props.studentId}</p>
     </footer>
   );
 }
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,11 +1,11 @@
-export default function TaskCard() {
+export default function TaskCard(props:{id:string, title:string, description:string, isDone:boolean}) {
   return (
     <div className="card mb-3">
       <div className="card-body">
         <div className="row align-items-center">
           <div className="col-lg-8">
-            <h5 className="card-title m-0">TODO: title</h5>
-            <p className="card-text m-0 text-muted">TODO: description</p>
+            <h5 className="card-title m-0"> {props.title}</h5>
+            <p className="card-text m-0 text-muted">{props.description}</p>
           </div>
           <div className="col-lg-2">
             <button className="btn btn-success w-100">Done</button>
